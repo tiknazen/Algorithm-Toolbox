4472ee9454bb5921b921ab663eee98f0f5ccf0ab
tiknazen <65359704+tiknazen@users.noreply.github.com>
2020-07-16 20:20:59 +0530

    Add files via upload

---
+++ lcm.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+long long int GCD(long long int A, long long int B) {
+    if(B==0)
+        return A;
+    else
+        return GCD(B, A % B);
+}
+int main(void) {
+  long long int a,b;
+  scanf("%lld %lld", &a, &b);
+  long long int l=(a*b)/GCD(a,b);
+  printf("%lld", l);
+  return 0;
+}
